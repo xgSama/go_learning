f5d709afd07a739f96050f4f6faa0354f33db39f
xgSama <china_cyzyc@163.com>
2021-04-21 10:34:03 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_learning
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.1
---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+/**
+ * @Author xgSama
+ * @Description:
+ * @Date 2021/4/21 10:25
+ */
+
+import (
+	"fmt"
+)
+
+func main() {
+	print("hello")
+	fmt.Print()
+}
